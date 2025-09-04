26708c306192066c7974d8f68846f058a0c68bb6
tiasapetsitter-collab <tiasapetsitter@gmail.com>
2025-09-04 20:44:26 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const cacheName = "bem-estar-cache-v1";
+const assets = [
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
