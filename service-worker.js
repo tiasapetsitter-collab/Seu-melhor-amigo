9a0b556ed162fe9add8f4d4d595518d28cdb2a4e
tiasapetsitter-collab <tiasapetsitter@gmail.com>
2025-09-05 22:22:52 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,8 @@
-const cacheName = "bem-estar-cache-v2";
-const assets = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png",
-  "/style.css",
-  "/app.js"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('bem-estar-v7').then(cache => cache.addAll([
+    'index.html','manifest.json','icon-192.png','icon-512.png'
+  ])));
 });
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
